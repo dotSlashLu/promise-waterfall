ae93ffdd7986ba10b8610d6f103f5377cc09c2fd
dotslash.lu <dotslash.lu@gmail.com>
2014-11-18 01:54:40 +0800

    New API draft

--- index.js
+++ index.js
@@ -1,57 +1,70 @@
-(function (globalVar){
+function isPromise(obj) {
+  return obj && typeof obj.then === 'function';
+}
 
-  function isPromise(object) {
-    // from Q.js
-    return object === Object(object) &&
-        typeof object.promiseDispatch === "function" &&
-        typeof object.inspect === "function";
+function waterfall(/*PromiseLib, list*/) {
+  var Promise, list;
+
+  if (arguments.length > 1) {
+    Promise = arguments[0];
+    list = arguments[1];
+
+    // invalid promise lib
+    if (!(typeof Promise == "function" && 
+      var p = new Promise(function(){}) && 
+      typeof p.then == "function")
+    )
+      throw new TypeError("Invalid Promise library");
+    
+    // bind to promise lib
+    Promise.prototype.waterfall = waterfall;
+  }
+  else {
+    list = arguments[0];
+  }
+  list = Array.prototype.slice.call(list);
+    
+}
+
+function _waterfall(list) {
+  // malformed argument
+  list = Array.prototype.slice.call(list);  // transform to Array
+  if (typeof list.reduce !== "function"     // update your javascript engine
+    || list.length < 1                      // empty array
+  ) {
+    throw new Error("Array with reduce function is needed.");
+    return;
   }
 
-  function waterfall(list) {
-    // malformed argument
-    if (!Array.isArray(list)                  // not an array
-      || typeof list.reduce !== "function"    // change your javascript engine
-      || list.length < 1                      // empty array
-    ) {
-      throw new Error("Array with reduce function is needed.");
-      return;
+  if (list.length == 1)
+    return list[0]()
+
+  return list.reduce(function(l, r){
+    // first round
+    // execute function and return promise
+    var isFirst = (l == list[0]);
+    if (isFirst) {
+      if (typeof l != "function")
+        throw new Error("List elements should be function to call.");
+
+      var lret = l();
+      if (!isPromise(lret))
+        throw new Error("Function return value should be a promise.");
+      else
+        return lret.then(r);
     }
+    
+    // other rounds
+    // l is a promise now
+    // priviousPromiseList.then(nextFunction)
+    else {
+      if (!isPromise(l))
+        throw new Error("Function return value should be a promise.");
+      else 
+        return l.then(r);
+    }
+  })
+}
 
-    if (list.length == 1)
-      return list[0]()
-
-    return list.reduce(function(l, r){
-      // first round
-      // execute function and return promise
-      var isFirst = (l == list[0]);
-      if (isFirst) {
-        if (typeof l != "function")
-          throw new Error("List elements should be function to call.");
-
-        var lret = l();
-        if (!isPromise(lret))
-          throw new Error("Function return value should be a promise.");
-        else
-          return lret.then(r);
-      }
-      
-      // other rounds
-      // l is a promise now
-      // priviousPromiseList.then(nextFunction)
-      else {
-        if (!isPromise(l))
-          throw new Error("Function return value should be a promise.");
-        else 
-          return l.then(r);
-      }
-    })
-  }
-
-  // export
-  var hasModule = (typeof module !== 'undefined' && module.exports);
-  if (hasModule)
-    module.exports = waterfall;
-  else
-    globalVar.waterfall = waterfall;
-
-}).call(this)
+// export
+module.exports = waterfall;
